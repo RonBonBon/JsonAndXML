8874a1ea7fd33765a59c8978fb69a934d2c935a2
hackerudroids <tomerjava@gmail.com>
2017-06-05 20:59:37 +0300

    Ynet 02

--- app/src/main/java/com/arichafamily/jsonandxml/YnetDataSource.java
+++ app/src/main/java/com/arichafamily/jsonandxml/YnetDataSource.java
@@ -1,7 +1,43 @@
 package com.arichafamily.jsonandxml;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class YnetDataSource {
-    
+    public interface OnYnetArrivedListener{
+        void onYnetArrived(List<Ynet> data);
+    }
+
+    public static void getYnet(OnYnetArrivedListener listener){
+        new AsyncTask<Void, Void, List<Ynet>>() {
+            @Override
+            protected List<Ynet> doInBackground(Void... params) {
+                try {
+                    String xml = IO.readWebSite("http://www.ynet.co.il/Integration/StoryRss2.xml");
+                    List<Ynet> data = parse(xml);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(List<Ynet> ynets) {
+
+            }
+        }.execute();
+    }
+
+    private static List<Ynet> parse(String xml) {
+        ArrayList<Ynet> data = new ArrayList<>();
+        Log.d("HackerU", xml);
+        return data;
+    }
+
     public static class Ynet{
         private String title;
         private String link;
