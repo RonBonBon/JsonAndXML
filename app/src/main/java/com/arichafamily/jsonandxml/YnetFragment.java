7fe86cd91a68105037770778c9f5dbad0b9561d6
hackerudroids <tomerjava@gmail.com>
2017-06-05 21:10:42 +0300

    Ynet 04

--- app/src/main/java/com/arichafamily/jsonandxml/YnetFragment.java
+++ app/src/main/java/com/arichafamily/jsonandxml/YnetFragment.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class YnetFragment extends Fragment {
+public class YnetFragment extends Fragment implements YnetDataSource.OnYnetArrivedListener {
 
 
     public YnetFragment() {
@@ -23,7 +26,17 @@ public class YnetFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ynet, container, false);
+        View v = inflater.inflate(R.layout.fragment_ynet, container, false);
+        YnetDataSource.getYnet(this);
+        return v;
     }
 
+    @Override
+    public void onYnetArrived(List<YnetDataSource.Ynet> data) {
+        if (data != null)
+            Toast.makeText(getActivity(), data.toString(), Toast.LENGTH_SHORT).show();
+        else {
+            Toast.makeText(getActivity(), "Error connecting to server", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
